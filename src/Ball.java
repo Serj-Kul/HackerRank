708ba6ded1b0adf5e290b8a8f231d60a4c7fb2eb
Serj-Kul <s.v.kulygin@gmail.com>
2017-04-22 14:47:35 +0300

    long to Biginteger

--- src/Ball.java
+++ src/Ball.java
@@ -1,20 +1,29 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Ball {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt(); // число ступенек
+        int N = scanner.nextInt();
 
-        long[] jump = new long[N + 1];
-        jump[0] = 1;
-        for (int i = 1; i <= N; i++) {
-            for (int j = max(0, i - 3); j < i; j++){
-                jump[i] += jump[j];
+        BigInteger[] steps = new BigInteger[N + 1];
+
+        if (N == 0) {
+            System.out.println(1);
+        } else if (N == 1) {
+            System.out.println(1);
+        } else if (N == 2) {
+            System.out.println(2);
+        } else if (N >= 3) {
+            steps[0] = BigInteger.valueOf(1);
+            steps[1] = BigInteger.valueOf(2);
+            steps[2] = BigInteger.valueOf(4);
+            int i = 3;
+            while (i < N) {
+                steps[i] = steps[i - 3].add((steps[i - 2].add(steps[i - 1])));
+                i++;
             }
+            System.out.println(steps[i - 1]);
         }
-        System.out.println(jump[N]);
-    }
-    private static int max(int a, int b) {
-        return (a > b) ? a : b;
     }
 }
